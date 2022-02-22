2586713fda09c101d24931329730e70100c593e8
CodeAkio <siqueira_2004@hotmail.com>
2022-02-22 14:25:34 -0300

    List and create breeds

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BreedsController } from './modules/breeds/breeds.controller';
+import { BreedsModule } from './modules/breeds/breeds.module';
+import { BreedsService } from './modules/breeds/breeds.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/dogbreed.db',
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    BreedsModule,
+  ],
+  controllers: [AppController, BreedsController],
+  providers: [AppService, BreedsService],
 })
 export class AppModule {}
---
+++ src/database/migrations/1645547773489-dog-breed.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class dogBreed1645547773489 implements MigrationInterface {
+    name = 'dogBreed1645547773489'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "dog_breed" ("Id" varchar PRIMARY KEY NOT NULL, "Name" varchar(300) NOT NULL, "PictureUrl" varchar(500) NOT NULL)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "dog_breed"`);
+    }
+
+}
